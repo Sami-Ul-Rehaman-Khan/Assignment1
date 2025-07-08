c276c20bf31143799af6b07b983898a454c77836
Sami-Ul-Rehaman-Khan <samiulrehamankhan@gmail.com>
2025-07-08 17:55:14 +0530

    Update and rename task.py to task1.py

--- task1.py
+++ task1.py
@@ -4,4 +4,4 @@ num2 = int(input("Enter the second Number:"))
 print("Addition:",num1+num2)
 print("Subtraction:",num1-num2)
 print("Multiplication:",num1*num2)
-print("Division:",num1/num2)5
+print("Division:",num1/num2)
